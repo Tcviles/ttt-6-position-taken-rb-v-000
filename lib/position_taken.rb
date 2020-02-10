aca108851b8680b7447e697772ae79abe5eafe87
Thomas Viles <thomas.c.viles.jr@gmail.com>
2020-02-10 00:55:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
+<<<<<<< HEAD
  board[index] == "X" || board[index] == "O"
+=======
+ ((board[index].strip.size > 0)||(!board[index].nil?))
+>>>>>>> 44a5ae5298d30c842b5413cbce9b0bc33dbd7e41
 end
